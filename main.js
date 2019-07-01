bc38bcc4cb850deb5650c7010d082329d07fe44b
ayoub <52166751+ayoubbenthabet@users.noreply.github.com>
2019-07-01 15:14:59 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function makeBold() {
+
+    document.getElementById("myText").style.fontWeight="Bold";
+    }
+
+function makeUnderline(){
+    
+    document.getElementById("myText").style.textDecoration="underline";
+
+    }
+function makeitalic(){
+    document.getElementById("myText").style.fontStyle="italic";
+}
+function aman() {
+    document.getElementById("myText").style.fontSize=document.getElementById('ks').value
+}
+function family() {
+    document.getElementById("myText").style.fontFamily=document.getElementById('rs').value;
+}
